b4374c5181b99549a780361e519b36c3fc7a3b78
tenwiz <nethe0002@gmail.com>
2018-04-26 12:05:06 +0400

    Revert "setup the sync for your component"
    This reverts commit ddbd90bf768f109459918adc639d39bdc48a2aab.

--- app/App.js
+++ app/App.js
@@ -3,15 +3,10 @@ import {AppContainer} from "./containers/App/AppContainer"
 import {Provider} from "react-redux"
 import configureStore from './configureStore'
 import { PersistGate } from 'redux-persist/integration/react'
-import codePush from "react-native-code-push"
 
 const { store, persistor } = configureStore()
-const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME
-}
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -22,5 +17,3 @@ class App extends Component {
     )
   }
 }
-
-export default codePush(codePushOptions)(App)
