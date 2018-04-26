33f19a074d5b10392374d7a48d7d2ecc86bebc9d
tenwiz <nethe0002@gmail.com>
2018-04-26 17:20:24 +0400

    migrate from Mocha to Jest because Mocha doesn't support es6

--- e2e/init.js
+++ e2e/init.js
@@ -1,10 +1,13 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
 
-before(async () => {
+// Set the default test timeout of 120s
+jest.setTimeout(120000);
+
+beforeAll(async () => {
   await detox.init(config);
 });
 
-after(async () => {
+afterAll(async () => {
   await detox.cleanup();
 });
