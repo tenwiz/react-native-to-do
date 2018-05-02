c1efbfd5fe6482fdfa1706d3edc73a1ffd60eb38
tenwiz <nethe0002@gmail.com>
2018-05-02 10:15:55 +0400

    test lint staged

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,12 +1,12 @@
-import {applyMiddleware, createStore, compose} from "redux";
-import thunk from "redux-thunk";
-import {persistReducer, persistStore} from "redux-persist";
+import { applyMiddleware, createStore, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import rootReducer from "./redux";
+import rootReducer from './redux'
 
 const persistConfig = {
   key: 'root',
-  storage,
+  storage
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -16,9 +16,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export default () => {
   let store = createStore(
     persistedReducer,
-    composeEnhancers(
-      applyMiddleware(thunk)
-    )
+    composeEnhancers(applyMiddleware(thunk))
   )
   let persistor = persistStore(store)
   return { store, persistor }
