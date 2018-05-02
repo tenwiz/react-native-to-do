92b933091a13044eb03ee61c6c7b5a4389ddbbc3
tenwiz <nethe0002@gmail.com>
2018-05-02 10:15:01 +0400

    test lint-staged

--- app/components/AddTodo/AddTodoMain.js
+++ app/components/AddTodo/AddTodoMain.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
-import {View} from 'react-native'
-import {CustomButton} from "../common/CustomButton";
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { CustomButton } from '../common/CustomButton'
 import { reduxForm, Field } from 'redux-form'
-import {InputField} from "../common/InputField";
-import {MUTILINE_INPUT_BORDER} from "../../resource/color";
-import {commonPadding, paddingValue} from "../../resource/constant";
-import {todoColor} from "../../resource/image/image";
-import {ColorOption} from "./ColorOption";
-import {CustomDatePicker} from "./CustomDatePicker";
-import {TEXT_BUTTON_ADD, TEXT_TASK_PLACEHOLDER} from "../../resource/text";
+import { InputField } from '../common/InputField'
+import { MUTILINE_INPUT_BORDER } from '../../resource/color'
+import { commonPadding, paddingValue } from '../../resource/constant'
+import { todoColor } from '../../resource/image/image'
+import { ColorOption } from './ColorOption'
+import { CustomDatePicker } from './CustomDatePicker'
+import { TEXT_BUTTON_ADD, TEXT_TASK_PLACEHOLDER } from '../../resource/text'
 
 /**
  * Design: ReplaceMe!
@@ -16,7 +16,7 @@ import {TEXT_BUTTON_ADD, TEXT_TASK_PLACEHOLDER} from "../../resource/text";
 class AddTodoMain extends Component {
   render() {
     return (
-      <View style={{flex:1, padding: paddingValue}}>
+      <View style={{ flex: 1, padding: paddingValue }}>
         <Field
           name={'task'}
           component={InputField}
@@ -25,24 +25,28 @@ class AddTodoMain extends Component {
           height={100}
           multiline
         />
-        <View style={commonPadding}/>
-        <Field
-          name={'deadline'}
-          component={CustomDatePicker}
-        />
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 32}}>
-          {
-            Object.keys(todoColor).map((color, index) => (
-              <Field
-                key={index}
-                name={'color'}
-                component={ColorOption}
-                color={color}
-              />
-            ))
-          }
+        <View style={commonPadding} />
+        <Field name={'deadline'} component={CustomDatePicker} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingVertical: 32
+          }}
+        >
+          {Object.keys(todoColor).map((color, index) => (
+            <Field
+              key={index}
+              name={'color'}
+              component={ColorOption}
+              color={color}
+            />
+          ))}
         </View>
-        <CustomButton btnText={TEXT_BUTTON_ADD} onPress={this.props.handleSubmit}/>
+        <CustomButton
+          btnText={TEXT_BUTTON_ADD}
+          onPress={this.props.handleSubmit}
+        />
       </View>
     )
   }
@@ -50,4 +54,4 @@ class AddTodoMain extends Component {
 
 export default reduxForm({
   form: 'addTodo'
-})(AddTodoMain);
+})(AddTodoMain)
